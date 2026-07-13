1aa9e9b7593f885ea22f365879983af386d1f816
tgiqfe <tqji31001@gmail.com>
2026-07-13 18:14:52 +0900

    Add ContextMenu items.

--- RegViewer/Lib/Converter/BoolToVisibilityConverter.cs
+++ RegViewer/Lib/Converter/BoolToVisibilityConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace RegViewer.Lib.Converter
 {
-    internal class BootToVisibilityConverter : IValueConverter
+    internal class BoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
--- RegViewer/Lib/Converter/BoolToVisibilityNegativeConverter.cs
+++ RegViewer/Lib/Converter/BoolToVisibilityNegativeConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace RegViewer.Lib.Converter
 {
-    internal class BootToVisibilityNegativeConverter : IValueConverter
+    internal class BoolToVisibilityNegativeConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
